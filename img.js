566cabc960d06a9de9704c0cc99e5ece7cf2ac16
Nixikaze <531254674@qq.com>
2017-03-16 20:27:15 +0800

    modefied js

--- img.js
+++ img.js
@@ -1,17 +1,20 @@
+// 原生http模块,用于请求文件或者创建服务器
 var http = require('http');
+// 原生fs模块,用于读写文件
 var fs = require('fs');
+// 调用第三方cheerio和mysql模块
 var cheerio = require('cheerio');
 var mysql = require('mysql');
-
+// 连接数据库
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'second'
-})
-
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'second'
+    })
+    // 执行链接
 connection.connect();
-
+// 用于获取需要被爬虫的网页DOM结构
 function download(url, callback) {
     http.get(url, function(res) {
         var data = '';
@@ -24,19 +27,21 @@ function download(url, callback) {
     })
 }
 
-download('http://www.nipic.com/index.html', function(data) {
+download('在此处输入需要抓包的网站链接', function(data) {
+    // cheerio处理网页信息
     var $ = cheerio.load(data);
     var imgarr = [];
+    // 选择图片元素并将图片地址保存到本地数据库
     $('img').each(function(index, item) {
-        console.log(item)
-        var src = $(item).attr('src');
-        connection.query('INSERT INTO `img`(`img`) VALUES ("' + src + '")', function(error, results, fields) {
-            if (error) throw error;
-        });
-        imgarr.push(src);
-        console.log(imgarr)
-    })
-
+            console.log(item)
+            var src = $(item).attr('src');
+            connection.query('INSERT INTO `img`(`img`) VALUES ("' + src + '")', function(error, results, fields) {
+                if (error) throw error;
+            });
+            imgarr.push(src);
+            console.log(imgarr)
+        })
+        // 执行图片下载
     downloadImg(imgarr);
 })
 
@@ -49,9 +54,13 @@ function downloadImg(imgarr) {
         res.pipe(writerStream);
         if (i < len) {
             i++;
+            // 遍历执行图片下载,每下载完一张图片再下载下一张
             downloadImg(imgarr);
         } else {
             return;
         }
     })
 }
+
+
+// 在nodejs中执行 node img.js即可运行程序
